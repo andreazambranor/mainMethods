3ec26d806b4bb2405ff8e86f34ea11607963b2bc
andreazambranor <azambranor.23@gmail.com>
2022-12-01 01:02:42 -0500

    Add project files.

---
+++ mainMethods/NewInt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethods
+{
+    public class NewInt
+    {
+        public int Value { get; set; }
+        public decimal Valuedec { get; set; }
+        public string Valuestr { get; set; }
+
+        public int Math1(NewInt one)
+        {
+            return one.Value+10;
+        }
+
+        public decimal Math2(NewInt one)
+        {
+            return Convert.ToInt32(one.Valuedec + 1.2M);
+        }
+
+        public string Math3(NewInt one)
+        {
+            Convert.ToInt32(one.Valuestr);
+            return one.Valuestr + 1;
+        }
+
+
+    }
+
+}
---
+++ mainMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethods
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NewInt one = new NewInt() { Value = 29, Valuedec = 29.9M, Valuestr = "30"};
+
+
+            Console.WriteLine("In 10 years, I will be "+ one.Math1(one));
+            Console.WriteLine("This must add decimals and return an integer: " + one.Math2(one));
+            Console.WriteLine("This must take a string, convert to int and return the result of a math operation: " + one.Math3(one));
+            Console.ReadLine();
+
+
+        }
+    }
+}
